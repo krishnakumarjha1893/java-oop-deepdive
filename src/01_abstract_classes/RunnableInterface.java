edfe898086e1549e34b1e49907dc4cf509373f20
ahj anhsirk <kjha1893@gmail.com>
2026-09-17 15:41:45 +0530

    Runnable ho ew actual looks like

---
+++ src/01_abstract_classes/RunnableInterface.java
@@ -0,0 +1,6 @@
+@FunctionalInterface
+Public interface Runnable{
+  public abstract void run();
+}
+
+// ye functional interface hi ho gya n kyunkki sirf ek hi method hai aur wo  v abastrract so aage ja ke apan lemda v esme use kr sakte
